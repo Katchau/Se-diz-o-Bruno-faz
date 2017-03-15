cc6d7ebd04d0df228b4820ea2146990835e5f88b
Jonas L <joaohccl@live.com.pt>
2017-03-15 12:29:25 +0000

    gitkraken é bosta
    :(

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+import java.io.IOException;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ola");
+		BackupFile lmao = null;
+		try {
+			lmao = new BackupFile("cebas.txt");
+		} catch (IOException e) {
+			System.err.println("ups i dit it ty bruno");
+		}
+		catch(Exception e){
+			System.err.println("file doesn't exist");
+		}
+		
+		lmao.createChunks();
+		
+		System.out.println("Welcome to the Jungle. We got fun and games");
+		
+		lmao.assembleFile();
 	}
-
 }
